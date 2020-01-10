ee6cf8f6f48931aa623c94491220f1e2f1dfaf1e
Deekshith Bommidi <deekshithbommidi@gmail.com>
2020-01-10 10:29:39 +0530

    resume uploaded

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -1,7 +1,24 @@
-$.getJSON('/data', function(d){
-    alert(JSON.stringify(d));
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-    $('#name').text(d.name);
-    $('#college').text(d.college);
-    $('#regno').text(d.regno);
-})
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('frontend'))//this line tells to serve js or css files from frontend folder
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+
+var student = {"name" : "Deekshith Bommidi",
+               "college" : "VNRVJIET",
+               "regno": "17071A12C6"}
+app.get('/data', (req, res) => res.json(student))   
+app.get('/resume', (req, res) => res.render(resume.html))
+
+
+//res.sendFile())
+//res.json())
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
