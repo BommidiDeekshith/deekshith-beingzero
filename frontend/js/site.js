cadf30b89d923790baacb457dba8c08b83df1072
BommidiDeekshith <42476096+BommidiDeekshith@users.noreply.github.com>
2020-01-10 10:31:59 +0530

    Resume site uploaded

--- frontend/js/site.js
+++ frontend/js/site.js
@@ -1,7 +1,25 @@
-$.getJSON('/data', function(d){
-//     alert(JSON.stringify(d));
 
-//     $('#name').text(d.name);
-//     $('#college').text(d.college);
-//     $('#regno').text(d.regno);
-})
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('frontend'))//this line tells to serve js or css files from frontend folder
+app.get('/', (req, res) => res.sendFile(__dirname+'/frontend/html/home.html'))
+
+app.get('/login', (req, res) => res.send('Login'))
+app.get('/register', (req, res) => res.send('Register'))
+
+var student = {"name" : "Deekshith Bommidi",
+               "college" : "VNRVJIET",
+               "regno": "17071A12C6"}
+app.get('/data', (req, res) => res.json(student))   
+app.get('/resume', (req, res) => res.render(resume.html))
+
+
+//res.sendFile())
+//res.json())
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
